858baf8651d8d8a67e28af5f23311fa6691f05ef
pureum <dlvnfma0501@naver.com>
2022-01-13 05:55:17 +0900

    ADD Destructor

--- module_04/ex03/AMateria.cpp
+++ module_04/ex03/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: plee <plee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 21:40:59 by plee              #+#    #+#             */
-/*   Updated: 2022/01/11 23:45:02 by plee             ###   ########.fr       */
+/*   Updated: 2022/01/13 05:48:41 by plee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 AMateria::AMateria(void) : _type("Default"){}
 AMateria::AMateria(std::string const & type) : _type(type) {}
 AMateria::AMateria(const AMateria& amateria) : _type(amateria._type) {}
+AMateria::~AMateria(void) {}
 
 AMateria& AMateria::operator=(const AMateria& amateria)
 {
